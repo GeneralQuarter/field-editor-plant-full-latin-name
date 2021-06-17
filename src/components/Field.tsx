de879bb1d25b6f058f170cd0b0cc576fa0f8547d
GeneralQuarter <x1q.g1x@gmail.com>
2021-06-17 14:10:25 +0200

    initial version

--- src/components/Field.tsx
+++ src/components/Field.tsx
@@ -1,16 +1,63 @@
 import React from 'react';
-import { Paragraph } from '@contentful/forma-36-react-components';
 import { FieldExtensionSDK } from '@contentful/app-sdk';
+import { SlugEditor } from '@contentful/field-editor-slug';
+import { useEffect } from 'react';
 
 interface FieldProps {
   sdk: FieldExtensionSDK;
 }
 
-const Field = (props: FieldProps) => {
-  // If you only want to extend Contentful's default editing experience
-  // reuse Contentful's editor components
-  // -> https://www.contentful.com/developers/docs/extensibility/field-editors/
-  return <Paragraph>Hello Entry Field Component</Paragraph>;
-};
+const Field = ({sdk}: FieldProps) => {
+  const field = sdk.field;
+
+  const genusField = sdk.entry.fields['genus'];
+  const speciesField = sdk.entry.fields['species'];
+  const varietyCultivarField = sdk.entry.fields['varietyCultivar'];
+
+  const updateFullLatinName = () => {
+    const genus = genusField.getValue() as string;
+    const species = speciesField.getValue() as string;
+    const varietyCultivar = varietyCultivarField.getValue() as string;
+
+    if (!genus || !species) {
+      field.setValue('');
+      return;
+    }
+
+    const newValue = `${genus} ${species}${varietyCultivar ? ` '${varietyCultivar}'` : ''}`;
+
+    if (field.getValue() === newValue) {
+      return;
+    }
+
+    field.setValue(newValue);
+  };
+
+  useEffect(() => {
+    const unsub = genusField.onValueChanged(() => {
+      updateFullLatinName();
+    });
+
+    return unsub;
+  });
+
+  useEffect(() => {
+    const unsub = speciesField.onValueChanged(() => {
+      updateFullLatinName();
+    });
+
+    return unsub;
+  });
+
+  useEffect(() => {
+    const unsub = varietyCultivarField.onValueChanged(() => {
+      updateFullLatinName();
+    });
+
+    return unsub;
+  });
+
+  return <SlugEditor baseSdk={sdk} field={field} isInitiallyDisabled={true} />
+}
 
 export default Field;
