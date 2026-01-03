eefae2ec6a576a04276e86e85fc216283c318086
GeneralQuarter <x1q.g1x@gmail.com>
2026-01-03 15:20:06 +0100

    fix: crash on plant card duplication due to lingui

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,11 @@
 import { GlobalStyles } from '@contentful/f36-components';
 import { SDKProvider } from '@contentful/react-apps-toolkit';
-
+import { i18n } from '@lingui/core';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+
+// TEMP FIX: lingui complains its not activated when duplicating plant card
+i18n.activate('en-US');
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
@@ -11,5 +14,5 @@ root.render(
   <SDKProvider>
     <GlobalStyles />
     <App />
-  </SDKProvider>
+  </SDKProvider>,
 );
